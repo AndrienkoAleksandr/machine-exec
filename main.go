c3f51fccfdbd926f4786a24277f73de9097dec88
Oleksandr Andriienko <oandriie@redhat.com>
2018-03-13 12:42:10 +0200

    Add handler to server client files
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main()  {
+import (
+	"flag"
+	"log"
+	"net/http"
+)
 
+var url, filesPath string
+
+func init() {
+	flag.StringVar(&url, "url", ":3333", "Host:Port address. ")
+	flag.StringVar(&filesPath, "client", "./client", "Path to the files to serve them.")
+}
+
+func main() {
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(filesPath)))
+
+	log.Printf("Staring file server on '%s'", url)
+
+	//serve files
+	err := http.ListenAndServe(url, nil)
+	if err != nil {
+		log.Fatalf("Failed to start exec machine server by address: '%s'", url)
+	}
 }
