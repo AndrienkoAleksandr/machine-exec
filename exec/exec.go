a61eac958503d2be8fc7b8360dd6b9aaecf3ba78
Oleksandr Andriienko <oandriie@redhat.com>
2018-03-14 12:10:52 +0200

    Code format.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- exec/exec.go
+++ exec/exec.go
@@ -1,28 +1,28 @@
 package exec
 
 import (
-	"fmt"
-	"golang.org/x/net/websocket"
-	"github.com/docker/docker/client"
-	"github.com/docker/docker/api/types"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/websocket"
 )
 
 var cli *client.Client = createDockerClient()
 
 type MachineIdentier struct {
 	machineName string
-	wsId string
+	wsId        string
 }
 
 type MachineExec struct {
 	identifier MachineIdentier `json:"identifier"`
-	cmd string `json:"string"`
-	pty bool `json:"bool"`
-	cols int `json:"cols"`
-	rows int `json:"rows"`
-	id int64 `json:"id"`
+	cmd        string          `json:"string"`
+	pty        bool            `json:"bool"`
+	cols       int             `json:"cols"`
+	rows       int             `json:"rows"`
+	id         int64           `json:"id"`
 }
 
 func createDockerClient() *client.Client {
@@ -37,7 +37,7 @@ func createDockerClient() *client.Client {
 func Create(ws *websocket.Conn) {
 	fmt.Println("create")
 
-	machineExec, err := parseMachineExec(ws);
+	machineExec, err := parseMachineExec(ws)
 	if err != nil {
 		fmt.Errorf(err.Error()) // todo fatality ?
 		return
@@ -71,12 +71,12 @@ func findContainer(identifier *MachineIdentier) (*types.Container, error) {
 func parseMachineExec(ws *websocket.Conn) (*MachineExec, error) {
 	buff := make([]byte, 8192)
 	if _, err := ws.Read(buff); err != nil {
-		return nil, errors.New("Failed to read machine exec body " + err.Error());
+		return nil, errors.New("Failed to read machine exec body " + err.Error())
 	}
 
 	machineExec := &MachineExec{}
 	if err := json.Unmarshal(buff, machineExec); err != nil {
-		return nil, errors.New("Failed to parse MachineExec " +  err.Error());
+		return nil, errors.New("Failed to parse MachineExec " + err.Error())
 	}
 
 	return machineExec, nil
