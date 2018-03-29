43953b2e2a37370d4e9af016dff52bf5d56c21e5
Oleksandr Andriienko <oandriie@redhat.com>
2018-03-29 12:38:13 +0300

    Improve compile scripts.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-$(CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .)
-
-if [ $? != 0 ]; then
-    echo "Failed to compile code";
-    exit 0;
-fi
+source ./compile.sh
 
 ./main
